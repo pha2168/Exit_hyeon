0e39d23be7844c1a174287b52558af630a2f15d4
Jiyeong0101 <139311671+Jiyeong0101@users.noreply.github.com>
2025-02-07 00:43:48 +0900

    Revert "25-0207"
    This reverts commit e6c60eac847b5a0341dda106ed281ec61183fdd3.

--- Assets/PHA/TetriminoBlock/Script/Block_TetriminoScript.cs
+++ Assets/PHA/TetriminoBlock/Script/Block_TetriminoScript.cs
@@ -4,22 +4,9 @@ using UnityEngine;
 
 public class Block_TetriminoScript : MonoBehaviour
 {
-    private QuestManager questManager; // QuestManager 인스턴스에 대한 참조
-
     public string GetTag()
     {
         return this.tag;
-    }
-
-    void Start()
-    {
-        // QuestManager 객체를 찾고 해당 컴포넌트를 가져옴
-        questManager = GameObject.FindObjectOfType<QuestManager>();
-
-        if (questManager == null)
-        {
-            Debug.LogError("QuestManager를 씬에서 찾을 수 없습니다."); // QuestManager가 없을 경우 에러 로그 출력
-        }
     }
 
     Block_TetriminoScript() 
@@ -30,19 +17,5 @@ public class Block_TetriminoScript : MonoBehaviour
     ~Block_TetriminoScript() 
     {
         //스테이더스 값 감소 로직
-    }
-
-    void OnDestroy()
-    {
-        // QuestManager가 null인지 확인하고 OnObjectDestroyed 호출
-        if (questManager != null && gameObject != null)
-        {
-            // 현재 오브젝트의 태그를 전달하여 퀘스트 진행
-            questManager.OnObjectDestroyed(gameObject.tag);
-        }
-        else
-        {
-            Debug.LogWarning("OnDestroy 호출 시 QuestManager가 null이거나 객체가 존재하지 않습니다.");
-        }
     }
 }
