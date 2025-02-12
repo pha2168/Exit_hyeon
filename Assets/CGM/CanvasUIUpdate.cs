aba1983dee3b019f7b1126967c1f569c70c735ae
ChoGM <148638359+ChoGM@users.noreply.github.com>
2025-02-13 00:33:33 +0900

    250213- 1
    250213- 1

---
+++ Assets/CGM/CanvasUIUpdate.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CanvasUIUpdate : MonoBehaviour
+{
+    void OnEnable()
+    {
+        // 캔버스 업데이트 강제 실행
+        Canvas.ForceUpdateCanvases();
+
+        // 레이아웃 그룹 강제 업데이트
+        foreach (var layout in GetComponentsInChildren<LayoutGroup>())
+        {
+            layout.enabled = false;
+            layout.enabled = true;
+        }
+    }
+
+}
