75172e9ff91902ec4ad52e544c034e50bf0bd546
Jiyeong0101 <139311671+Jiyeong0101@users.noreply.github.com>
2025-02-14 22:45:14 +0900

    25_0214_3

--- Assets/CJY/Scripts/Setting/AudioManager.cs
+++ Assets/CJY/Scripts/Setting/AudioManager.cs
@@ -1,21 +1,17 @@
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 
 public class AudioManager : MonoBehaviour
 {
     public static AudioManager Instance { get; private set; }
 
-    public AudioSource bgmAudioSource;
-    public AudioSource sfxAudioSource;
+    public AudioMixer audioMixer; // 오디오 믹서 추가
+    public AudioSource bgmAudioSource; // BGM만 AudioSource 유지
 
-    private float bgmVolume;
-    private float sfxVolume;
-
-    public Slider bgmSlider;  // BGM 슬라이더
-    public Slider sfxSlider;  // SFX 슬라이더
-
+    public Slider bgmSlider;
+    public Slider sfxSlider;
     public GameObject AudioSetting;
 
     private void Awake()
@@ -26,81 +22,58 @@ public class AudioManager : MonoBehaviour
         }
         else
         {
-            Destroy(gameObject); // 이미 존재하는 인스턴스가 있으면 파괴
+            Destroy(gameObject);
             return;
         }
 
-        // AudioSource가 없다면 찾기
-        if (bgmAudioSource == null) bgmAudioSource = GetComponent<AudioSource>();
-        if (sfxAudioSource == null) sfxAudioSource = GetComponent<AudioSource>();
-
-        // 게임 데이터를 로드 (PlayerPrefs에서 직접 불러오기)
         LoadVolumeSettings();
     }
 
     private void Start()
     {
-        // 씬이 로드될 때마다 BGM 설정
         SceneManager.sceneLoaded += (scene, mode) => PlaySceneBGM(scene.name);
 
-        // 게임 시작 시 볼륨을 설정
         ApplyVolumeSettings();
 
-        // 슬라이더 값 초기화
-        if (bgmSlider != null) bgmSlider.value = bgmVolume;
-        if (sfxSlider != null) sfxSlider.value = sfxVolume;
+        if (bgmSlider != null) bgmSlider.value = PlayerPrefs.GetFloat("BGMVolume", 0.5f);
+        if (sfxSlider != null) sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 0.5f);
 
-        // 슬라이더 값이 바뀔 때마다 저장되도록 설정
-        if (bgmSlider != null)
-            bgmSlider.onValueChanged.AddListener(SetBGMVolume);
-        if (sfxSlider != null)
-            sfxSlider.onValueChanged.AddListener(SetSFXVolume);
+        if (bgmSlider != null) bgmSlider.onValueChanged.AddListener(SetBGMVolume);
+        if (sfxSlider != null) sfxSlider.onValueChanged.AddListener(SetSFXVolume);
     }
 
     private void LoadVolumeSettings()
     {
-        // PlayerPrefs에서 직접 값 불러오기
-        bgmVolume = PlayerPrefs.GetFloat("BGMVolume", 0.5f); // 기본값 0.5
-        sfxVolume = PlayerPrefs.GetFloat("SFXVolume", 0.5f); // 기본값 0.5
+        float bgmVolume = PlayerPrefs.GetFloat("BGMVolume", 0.5f);
+        float sfxVolume = PlayerPrefs.GetFloat("SFXVolume", 0.5f);
+
+        SetBGMVolume(bgmVolume);
+        SetSFXVolume(sfxVolume);
     }
 
     private void ApplyVolumeSettings()
     {
-        if (bgmAudioSource != null)
+        if (audioMixer != null)
         {
-            bgmAudioSource.volume = bgmVolume;
-        }
-
-        if (sfxAudioSource != null)
-        {
-            sfxAudioSource.volume = sfxVolume;
+            audioMixer.SetFloat("BGMVolume", Mathf.Log10(PlayerPrefs.GetFloat("BGMVolume", 0.5f)) * 20);
+            audioMixer.SetFloat("SFXVolume", Mathf.Log10(PlayerPrefs.GetFloat("SFXVolume", 0.5f)) * 20);
         }
     }
 
     public void SetBGMVolume(float volume)
     {
-        bgmVolume = volume;
-        if (bgmAudioSource != null)
-        {
-            bgmAudioSource.volume = bgmVolume;
-        }
-
-        // PlayerPrefs에 저장
-        PlayerPrefs.SetFloat("BGMVolume", bgmVolume);
-        PlayerPrefs.Save();  // 변경 사항을 저장
+        float dB = Mathf.Log10(Mathf.Max(volume, 0.0001f)) * 20;
+        audioMixer.SetFloat("BGMVolume", dB);
+        PlayerPrefs.SetFloat("BGMVolume", volume);
+        PlayerPrefs.Save();
     }
 
     public void SetSFXVolume(float volume)
     {
-        sfxVolume = volume;
-        if (sfxAudioSource != null)
-        {
-            sfxAudioSource.volume = sfxVolume;
-        }
-
-        // PlayerPrefs에 저장
-        PlayerPrefs.SetFloat("SFXVolume", sfxVolume);
-        PlayerPrefs.Save();  // 변경 사항을 저장
+        float dB = Mathf.Log10(Mathf.Max(volume, 0.0001f)) * 20;
+        audioMixer.SetFloat("SFXVolume", dB);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+        PlayerPrefs.Save();
     }
 
     private void PlaySceneBGM(string sceneName)
@@ -113,13 +86,14 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    public void AudioSettingOn()
+    public void PlaySFX(AudioClip clip)
     {
-        AudioSetting.SetActive(true);
+        if (clip != null)
+        {
+            AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position, PlayerPrefs.GetFloat("SFXVolume", 0.5f));
+        }
     }
 
-    public void AudioSettingOff()
-    {
-        AudioSetting.SetActive(false);
-    }
+    public void AudioSettingOn() => AudioSetting.SetActive(true);
+    public void AudioSettingOff() => AudioSetting.SetActive(false);
 }
